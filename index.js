636bd5bc7dce7c8d57992991da99356cafec1e04
viniciustlima <vinitlima2007.14@gmail.com>
2022-01-28 23:10:33 -0300

    second commit

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ app.get('/', (req, res) => {
 	return res.send(`your ip address is: ${ip.address()}`);
 });
 
-app.listen(3333, console.log('sever running'));
+app.listen(3333, console.log('server running'));
